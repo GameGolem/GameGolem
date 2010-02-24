e8f9d3ba6883c4e4e105e498f3e288264f442748
RycochetTM <RycochetTM@98a03d04-1f35-11df-8982-67e453f37615>
2010-02-24 06:49:29 +0000

    Just noticed no way to browse the "compiled" scripts if GM is installed so renamed them.

---
+++ _minuser.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name           Rycochet's Castle Age Golem
+// @namespace      golem
+// @description    Auto player for castle age game
+// @version        11
+// @include        http*://apps.*facebook.com/castle_age/*
+// @require        http://cloutman.com/jquery-latest.min.js
+// @require        http://cloutman.com/jquery-ui-latest.min.js
+// ==/UserScript==
+// -- @include        http://www.facebook.com/common/error.html
+// -- @include        http://www.facebook.com/reqs.php#confirm_46755028429_0
+// -- @include        http://www.facebook.com/home.php
+// -- @include        http://www.facebook.com/home.php*filter=app_46755028429*
