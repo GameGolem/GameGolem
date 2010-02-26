ea8d7d9ab28c2d60458ba5976e0f1db9bb44d4d3
RycochetTM <RycochetTM@98a03d04-1f35-11df-8982-67e453f37615>
2010-02-26 08:54:55 +0000

    Version 13

--- main.js
+++ main.js
@@ -6,7 +6,7 @@
 var debug = true;
 
 // Shouldn't touch
-var VERSION = 12;
+var VERSION = 13;
 var APP = '46755028429';
 var PREFIX = 'golem'+APP+'_';
 var userID = unsafeWindow.Env.user; // Facebook userid
