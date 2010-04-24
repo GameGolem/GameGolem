87a66e168ea12aaa51cace42c612a657d4cd5072
RycochetTM <RycochetTM@98a03d04-1f35-11df-8982-67e453f37615>
2010-04-24 22:38:38 +0000

    worker_alchemy.js - New "Use Summon Ingredients" option.

--- worker_alchemy.js
+++ worker_alchemy.js
@@ -16,7 +16,8 @@ Alchemy.data = {
 
 Alchemy.option = {
 	perform:true,
-	hearts:false
+	hearts:false,
+	summon:false
 };
 
 Alchemy.runtime = {
@@ -32,22 +33,31 @@ Alchemy.display = [
 		id:'hearts',
 		label:'Use Battle Hearts',
 		checkbox:true
+	},{
+		id:'summon',
+		label:'Use Summon Ingredients',
+		checkbox:true
 	}
 ];
 
 Alchemy.parse = function(change) {
-	var ingredients = Alchemy.data.ingredients = {}, recipe = Alchemy.data.recipe = {};
+	var ingredients = this.data.ingredients = {}, recipe = this.data.recipe = {};
 	$('div.ingredientUnit').each(function(i,el){
 		var name = $('img', el).attr('src').filepart();
 		ingredients[name] = $(el).text().regex(/x([0-9]+)/);
 	});
 	$('div.alchemyQuestBack,div.alchemyRecipeBack,div.alchemyRecipeBackMonster').each(function(i,el){
 		var me = {}, title = $('div.recipeTitle', el).text().trim().replace('RECIPES: ','');
-		if (title.indexOf(' (')>0) {title = title.substr(0, title.indexOf(' ('));}
-		if ($(el).hasClass('alchemyQuestBack')) {me.type = 'Quest';}
-		else if ($(el).hasClass('alchemyRecipeBack')) {me.type = 'Recipe';}
-		else if ($(el).hasClass('alchemyRecipeBackMonster')) {me.type = 'Summons';}
-		else {me.type = 'wtf';}
+		if (title.indexOf(' (')>0) {
+			title = title.substr(0, title.indexOf(' ('));
+		}
+		if ($(el).hasClass('alchemyQuestBack')) {
+			me.type = 'Quest';
+		} else if ($(el).hasClass('alchemyRecipeBack')) {
+			me.type = 'Recipe';
+		} else if ($(el).hasClass('alchemyRecipeBackMonster')) {
+			me.type = 'Summons';
+		}
 		me.ingredients = {};
 		$('div.recipeImgContainer', el).parent().each(function(i,el){
 			var name = $('img', el).attr('src').filepart();
@@ -59,11 +69,20 @@ Alchemy.parse = function(change) {
 
 Alchemy.update = function() {
 	var best = null, recipe = this.data.recipe, r, i;
+	if (!this.option.summon) {
+		for (r in recipe) {
+			if (recipe[r].type === 'Summons') {
+				for (i in recipe[r].ingredients) {
+					this.data.ingredients[i] = 0;
+				}
+			}
+		}
+	}
 	for (r in recipe) {
 		if (recipe[r].type === 'Recipe') {
 			best = r;
 			for (i in recipe[r].ingredients) {
-				if ((!Alchemy.option.hearts && i === 'raid_hearts.gif') || recipe[r].ingredients[i] > (Alchemy.data.ingredients[i] || 0)) {
+				if ((!this.option.hearts && i === 'raid_hearts.gif') || recipe[r].ingredients[i] > (this.data.ingredients[i] || 0)) {
 					best = null;
 					break;
 				}
@@ -75,15 +94,15 @@ Alchemy.update = function() {
 };
 
 Alchemy.work = function(state) {
-	if (!Alchemy.option.perform || !Alchemy.runtime.best) {
+	if (!this.option.perform || !this.runtime.best) {
 		return false;
 	}
 	if (!state || !Page.to('keep_alchemy')) {
 		return true;
 	}
-	debug('Alchemy: Perform - '+Alchemy.runtime.best);
+	debug('Alchemy: Perform - ' + this.runtime.best);
 	$('div.alchemyRecipeBack').each(function(i,el){
-		if($('div.recipeTitle', el).text().indexOf(Alchemy.runtime.best) >= 0) {
+		if($('div.recipeTitle', el).text().indexOf(this.runtime.best) >= 0) {
 			Page.click($('input[type="image"]', el));
 			return true;
 		}
