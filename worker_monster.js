5c6f31306d1aa36752df5ce7b79ee923d511c28b
venjiance <venjiance@98a03d04-1f35-11df-8982-67e453f37615>
2010-05-06 14:17:26 +0000

    worker_monster.js - Really fixed bug in finding damage done by players on raid pages.

--- worker_monster.js
+++ worker_monster.js
@@ -471,10 +471,13 @@ Monster.parse = function(change) {
 			monster.damage_total = 0;
 			monster.damage = {};
 			$('td.dragonContainer table table a[href^="http://apps.facebook.com/castle_age/keep.php?user="]').each(function(i,el){
-				var user = $(el).attr('href').regex(/user=([0-9]+)/i), tmp = $(el).parent().parent().next().text().replace(/[^0-9\/]/g,''), dmg = tmp.regex(/([0-9]+)/), fort = tmp.regex(/\/([0-9]+)/);
-				if (monster.raid){
-                                    tmp = $(el).parent().next().text().replace(/[^0-9\/]/g,''), dmg = tmp.regex(/([0-9]+)/), fort = tmp.regex(/\/([0-9]+)/);
-                                }
+				var user = $(el).attr('href').regex(/user=([0-9]+)/i);
+				if (types[type].raid){
+				    var tmp = $(el).parent().next().text().replace(/[^0-9\/]/g,'');
+                                } else {
+				    var tmp = $(el).parent().parent().next().text().replace(/[^0-9\/]/g,'');
+				}
+				var dmg = tmp.regex(/([0-9]+)/), fort = tmp.regex(/\/([0-9]+)/);
 				monster.damage[user]  = (fort ? [dmg, fort] : [dmg]);
 				monster.damage_total += dmg;
 			});
