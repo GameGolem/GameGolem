86a82e7f371e55e6bd37dea4581ebbeb93fe1192
venjiance <venjiance@98a03d04-1f35-11df-8982-67e453f37615>
2010-05-26 08:31:02 +0000

    worker_+page.js - Fix regression introduced in commit r443

--- _normal.user.js
+++ _normal.user.js
@@ -1611,7 +1611,7 @@ Page.click = function(el) {
 	}
 	var e = document.createEvent("MouseEvents");
 	e.initEvent("click", true, true);
-	$(el).get(0).wrappedJSObject ? $(el).get(0).wrappedJSObject : $(el).get(0).dispatchEvent(e);
+	$(el).get(0).wrappedJSObject ? $(el).get(0).wrappedJSObject.dispatchEvent(e) : $(el).get(0).dispatchEvent(e);
 	this.clear();
 	this.lastclick = el;
 	this.when = Date.now();
