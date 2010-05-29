c1a186f8909c323d60594ceae83c0064c63b76ef
haploaw <haploaw@98a03d04-1f35-11df-8982-67e453f37615>
2010-05-29 08:09:55 +0000

    CA_Golem.xml -Corrected error in revision number finding.
    Updated revision numbers.

--- _normal.user.js
+++ _normal.user.js
@@ -15,7 +15,7 @@
 // 
 // For the unshrunk Work In Progress version (which may introduce new bugs)
 // - http://game-golem.googlecode.com/svn/trunk/_normal.user.js
-var revision = "482";
+var revision = "484";
 // User changeable
 var show_debug = true;
 
