575dd317239f048466d30f084e9d80fdcdb4cdba
crissman <crissman@98a03d04-1f35-11df-8982-67e453f37615>
2010-06-08 15:04:12 +0000

    Updated News worker to include total War Points change.

--- _normal.user.js
+++ _normal.user.js
@@ -6603,10 +6603,10 @@ You lost the war, taking 10 damage and losing $0. You have lost 4 War Points!
 */
 News.parse = function(change) {
 	if (change) {
-		var xp = 0, bp = 0, win = 0, lose = 0, deaths = 0, cash = 0, i, j, list = [], user = {}, order, last_time = this.runtime.last;
+		var xp = 0, bp = 0, wp = 0, win = 0, lose = 0, deaths = 0, cash = 0, i, j, list = [], user = {}, order, last_time = this.runtime.last;
 		News.runtime.last = Date.now();
 		$('#app'+APPID+'_battleUpdateBox .alertsContainer .alert_content').each(function(i,el) {
-			var uid, txt = $(el).text().replace(/,/g, ''), title = $(el).prev().text(), days = title.regex(/([0-9]+) days/i), hours = title.regex(/([0-9]+) hours/i), minutes = title.regex(/([0-9]+) minutes/i), seconds = title.regex(/([0-9]+) seconds/i), time, my_xp = 0, my_bp = 0, my_cash = 0;
+			var uid, txt = $(el).text().replace(/,/g, ''), title = $(el).prev().text(), days = title.regex(/([0-9]+) days/i), hours = title.regex(/([0-9]+) hours/i), minutes = title.regex(/([0-9]+) minutes/i), seconds = title.regex(/([0-9]+) seconds/i), time, my_xp = 0, my_bp = 0, my_wp = 0, my_cash = 0;
 			time = Date.now() - ((((((((days || 0) * 24) + (hours || 0)) * 60) + (minutes || 59)) * 60) + (seconds || 59)) * 1000);
 			if (txt.regex(/You were killed/i)) {
 				deaths++;
@@ -6619,6 +6619,7 @@ News.parse = function(change) {
 					user[uid].lose++;
 					my_xp = txt.regex(/([0-9]+) experience/i);
 					my_bp = txt.regex(/([0-9]+) Battle Points!/i);
+					my_wp = txt.regex(/([0-9]+) War Points!/i);
 					my_cash = txt.regex(/\$([0-9]+)/i);
 					result = 'win';
 				} else {
@@ -6626,14 +6627,16 @@ News.parse = function(change) {
 					user[uid].win++;
 					my_xp = 0 - txt.regex(/([0-9]+) experience/i);
 					my_bp = 0 - txt.regex(/([0-9]+) Battle Points!/i);
+					my_wp = 0 - txt.regex(/([0-9]+) War Points!/i);
 					my_cash = 0 - txt.regex(/\$([0-9]+)/i);
 					result = 'loss';
 				}
 				if (time > last_time) {
-//					debug('Add to History (+battle): exp = '+my_xp+', bp = '+my_bp+', income = '+my_cash);
+//					debug('Add to History (+battle): exp = '+my_xp+', bp = '+my_bp+', wp = '+my_wp+', income = '+my_cash);
 					time = Math.floor(time / 3600000);
 					History.add([time, 'exp+battle'], my_xp);
 					History.add([time, 'bp+battle'], my_bp);
+					History.add([time, 'wp+battle'], my_wp);
 					History.add([time, 'income+battle'], my_cash);
 					switch (result) {
 						case 'win':
@@ -6646,6 +6649,7 @@ News.parse = function(change) {
 				}
 				xp += my_xp;
 				bp += my_bp;
+				wp += my_wp;
 				cash += my_cash;
 				
 			}
@@ -6655,6 +6659,7 @@ News.parse = function(change) {
 			list.push('You ' + (xp >= 0 ? 'gained <span class="positive">' : 'lost <span class="negative">') + addCommas(Math.abs(xp)) + '</span> experience points.');
 			list.push('You ' + (cash >= 0 ? 'gained <span class="positive">' : 'lost <span class="negative">') + '<b class="gold">$' + addCommas(Math.abs(cash)) + '</b></span>.');
 			list.push('You ' + (bp >= 0 ? 'gained <span class="positive">' : 'lost <span class="negative">') + addCommas(Math.abs(bp)) + '</span> Battle Points.');
+			list.push('You ' + (wp >= 0 ? 'gained <span class="positive">' : 'lost <span class="negative">') + addCommas(Math.abs(wp)) + '</span> War Points.');
 			list.push('');
 			user = sortObject(user, function(a,b){return (user[b].win + (user[b].lose / 100)) - (user[a].win + (user[a].lose / 100));});
 			for (i in user) {
