e7d68e254cd0c6f5d0ffac718dd4b63b7b8d3980
crissman <crissman@98a03d04-1f35-11df-8982-67e453f37615>
2010-06-08 11:48:22 +0000

    Merged

--- _normal.user.js
+++ _normal.user.js
@@ -15,7 +15,6 @@
 // 
 // For the unshrunk Work In Progress version (which may introduce new bugs)
 // - http://game-golem.googlecode.com/svn/trunk/_normal.user.js
-var revision = (541+1);
 // User changeable
 var show_debug = true;
 
