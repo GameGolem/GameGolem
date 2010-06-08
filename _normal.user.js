478768e8afd4e9e1077f7adf6cb8523f77e6db83
venjiance <venjiance@98a03d04-1f35-11df-8982-67e453f37615>
2010-06-08 16:31:51 +0000

    _normal.user.js - Fix revision _normal_chrome.user.js - Fix revision
    @crissman: could you update your build.bat, please?

--- _normal.user.js
+++ _normal.user.js
@@ -15,6 +15,7 @@
 // 
 // For the unshrunk Work In Progress version (which may introduce new bugs)
 // - http://game-golem.googlecode.com/svn/trunk/_normal.user.js
+var revision = (548+1);
 // User changeable
 var show_debug = true;
 
--- _normal_chrome.user.js
+++ _normal_chrome.user.js
@@ -1,3 +1,21 @@
+// ==UserScript==
+// @name		Rycochet's Castle Age Golem
+// @namespace	golem
+// @description	Auto player for castle age game
+// @license		GNU Lesser General Public License; http://www.gnu.org/licenses/lgpl.html
+// @version		31.1
+// @include		http://apps.facebook.com/castle_age/*
+// @include		http://apps.facebook.com/reqs.php
+// @require		http://cloutman.com/jquery-latest.min.js
+// @require		http://cloutman.com/jquery-ui-latest.min.js
+// ==/UserScript==
+// 
+// For the source code please check the sourse repository
+// - http://code.google.com/p/game-golem/
+// 
+// For the unshrunk Work In Progress version (which may introduce new bugs)
+// - http://game-golem.googlecode.com/svn/trunk/_normal.user.js
+var revision = (548+1);
 /*!
  * jQuery JavaScript Library v1.4.2
  * http://jquery.com/
