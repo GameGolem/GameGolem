bc3c642d00cbf7b6cbb7b3b782f726227f299e1f
RycochetTM <RycochetTM@98a03d04-1f35-11df-8982-67e453f37615>
2010-06-16 16:24:56 +0000

    build.sh - Updated for paths, still needs the chrome packed extension to be coded. CA_Golem.xml - netbeans config just removed caap version.

--- build.sh
+++ build.sh
@@ -27,6 +27,14 @@ js_compiler="../compiler.jar"
 update_firefox="No"
 
 
+# Generate Google Chrome extension
+#
+# Set it to "Yes" to generate the packed Google Chrmoe extension.
+# You must obtain the GameGolem.pem file from Rycochet for this.
+#
+build_chrome="No"
+
+
 # Generate _release.user.js
 #
 # Set it to "Yes" to generate the minimised version.
@@ -45,8 +53,9 @@ else
     vcs="git svn"
 fi
 rev=`LANG=C $vcs info . | awk '/^Revision:/{print $2}'`
-sed "s/\\\$WCREV\\\$/$rev/" _head_tortoise.tmpl > _head_tortoise.js
-sed "s/\\\$WCREV\\\$/$rev/" _chrome_manifest.tmpl > chrome/manifest.json
+sed "s/\\\$WCREV\\\$/$rev/" _head_tortoise.tmpl > _head_revision.js
+sed "s/\\\$WCREV\\\$/$rev/" chrome/manifest.tmpl > chrome/GameGolem/manifest.json
+sed "s/\\\$WCREV\\\$/$rev/" chrome/update.tmpl > chrome/update.xml
 
 ### generate _normal.user.js ###
 echo "Joining files into _normal.user.js"
@@ -59,29 +68,18 @@ cat _head*.js \
     $(ls -1 worker_*.js | grep -v "\+") \
     > _normal.user.js
 
-### Google Chrome copy (unpacked extension) ###
-echo "Copying to chrome/golem.user.js"
-cp _normal.user.js chrome/golem.user.js
+### Google Chrome extension (unpacked) ###
+echo "Copying to chrome/GameGolem/golem.user.js"
+cp _normal.user.js chrome/GameGolem/golem.user.js
 
-### generate _normal_caap.user.js ###
-echo "Joining files into _normal_caap.user.js"
-cat _normal.user.js \
-    worker++caap.js \
-    > _normal_caap.user.js
-
-### Caap/Golem Google Chrome copy (unpacked extension) ###
-echo "Copying to chrome_caap/golem.user.js"
-cp chrome/manifest.json chrome_caap/manifest.json
-cp _normal_caap.user.js chrome_caap/golem.user.js
-
-### INSTALLED VERSION ###
-if [ "$update_firefox" = "Yes" ]; then
-    echo "Installing new version to Firefox"
-    for script in $HOME/.mozilla/firefox/*.default/gm_scripts/rycochets_castle_age_gol/rycochets_castle_age_gol.user.js; do
-        if [ -f "$script" ]; then
-            cat _normal.user.js > "$script"
-        fi
-    done
+### GOOGLE CHROME EXTENSION ###
+if [ "$build_chrome" = "Yes" ]; then
+    echo "Would create Google Chrome extension, but it's not been written yet"
+# To build the "proper" chrome extension you need Chrome installed
+# *NOTE*: Chrome *CANNOT* be running - http://code.google.com/p/chromium/issues/detail?id=22901
+# To get the GameGolem.pem file please ask Rycochet - and don't share it!!!
+# Needs to be written properly -  basic syntax (might need absolute paths)
+### chrome.exe --no-message-box --pack-extension=chrome\GameGolem --pack-extension-key=chrome\GameGolem.pem
 fi
 
 ### MINIMISED VERSION ###
@@ -95,4 +93,14 @@ if [ "$build_release" = "Yes" ]; then
     else
       echo "Error: missing js compiler."
     fi
+fi
+
+### INSTALLED VERSION ###
+if [ "$update_firefox" = "Yes" ]; then
+    echo "Installing new version to Firefox"
+    for script in $HOME/.mozilla/firefox/*.default/gm_scripts/rycochets_castle_age_gol/rycochets_castle_age_gol.user.js; do
+        if [ -f "$script" ]; then
+            cat _normal.user.js > "$script"
+        fi
+    done
 fi
