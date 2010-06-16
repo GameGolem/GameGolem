33ddf1fcfc618219edcbb65d3fe875dc1e5cabc0
RycochetTM <RycochetTM@98a03d04-1f35-11df-8982-67e453f37615>
2010-06-16 07:31:46 +0000

    *** BUILD SCRIPT CHANGES *** * - Chrome extension (packed) support - talk to Rycochet to build (unpacked works for testing).

--- _normal_caap.user.js
+++ _normal_caap.user.js
@@ -17,7 +17,7 @@
 // 
 // For the unshrunk Work In Progress version (which may introduce new bugs)
 // - http://game-golem.googlecode.com/svn/trunk/_normal.user.js
-var revision = (608+1);
+var revision = (609+1);
 // User changeable
 var show_debug = true;
 
