a2c8d56e939a725ffce29cf180891711e1190888
haploaw <haploaw@98a03d04-1f35-11df-8982-67e453f37615>
2010-06-25 23:21:38 +0000

    Updating rev number. Need to determine why Netbeans using build.bat doesn't update rev properly.

--- _normal.user.js
+++ _normal.user.js
@@ -17,7 +17,7 @@
 // 
 // For the unshrunk Work In Progress version (which may introduce new bugs)
 // - http://game-golem.googlecode.com/svn/trunk/_normal.user.js
-var revision = 641;
+var revision = 642;
 // User changeable
 var show_debug = true;
 
