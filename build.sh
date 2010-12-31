0425d17408f42ff7d1c03030fdae7ac745ba044f
lurgid@hotmail.com <lurgid@hotmail.com@98a03d04-1f35-11df-8982-67e453f37615>
2010-12-31 07:24:36 +0000

    build.sh - build from manifest file

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
-#!/bin/sh 
+#!/bin/sh
 #
 # Build script for Game-Golem
 
 # Configuration file location
-conf="$(dirname $0)/build.conf" 
+conf="$(dirname $0)/build.conf"
 
 # Create default config file if it doesn't exists
 if [ ! -f "$conf" ]; then
@@ -19,14 +19,14 @@ if [ ! -f "$conf" ]; then
 workdir="$(dirname $0)"
 
 # Path of js compiler
-# 
+#
 # Required to build the minimized version. You can download it from
 # http://code.google.com/closure/compiler/
 #
 js_compiler="../compiler.jar"
 
 # Path of chrome/chromium
-# 
+#
 # Required to build the packed Google Chrome extension.
 # Will be detected automatically if you have chromium-browser
 # in your PATH
@@ -73,7 +73,11 @@ build_release="No"
 cd "$workdir"
 
 # Remove old stuff
-rm -f _normal.user.js _min.user.js chrome/GameGolem/* _head_revision.js _head_tortoise.js _head_version.js _version.js
+echo "Removing old build files."
+rm -f _normal.user.js _min.user.js _head_revision.js _head_tortoise.js _head_version.js _version.js main.js
+rm -f chrome/_version.js chrome/update.xml
+rm -rf chrome/GameGolem/*
+[ "$1" = "clean" ] && exit 1
 
 # Compute revision number
 if [ -d .svn ]; then
@@ -82,36 +86,76 @@ else
     vcs="git svn"
 fi
 rev=`LANG=C $vcs info . | awk '/^Revision:/{print $2 + 1}'`
-ver=`cat _version.txt`
+file=_version.txt
+if [ -f "$file" ] ; then
+    ver=`cat "$file"`
+else
+    echo "Error: missing $file"
+    rc=1
+fi
+head=_head.tmpl
+manifest=_manifest.txt
+rc=0
 
-sed "s/\\\$REV\\\$/$rev/g;s/\\\$VER\\\$/$ver/g" _version.tmpl > _version.js
-sed "s/\\\$REV\\\$/$rev/g;s/\\\$VER\\\$/$ver/g" main.tmpl > main.js
+for file in _version.tmpl main.tmpl ; do
+    if [ -f "$file" ] ; then
+        sed "s/\\\$REV\\\$/$rev/g;s/\\\$VER\\\$/$ver/g" $file > `basename $file .tmpl`.js
+    else
+        echo "Error: missing $file"
+        rc=1
+    fi
+done
+[ 0 = "$rc" ] || exit $rc
 
 ### generate _normal.user.js ###
-echo "Joining files into _normal.user.js"
-sed "s/\\\$REV\\\$/$rev/g;s/\\\$VER\\\$/$ver/g" _head.tmpl > _normal.user.js
-cat main.js \
-    utility.js \
-    worker.js \
-    $(ls -1 worker_+*.js) \
-    $(ls -1 worker_*.js | grep -v "\+") \
-    >> _normal.user.js
+out=_normal.user.js
+echo "Joining files into $out"
+if [ -f "$head" ] ; then
+    sed "s/\\\$REV\\\$/$rev/g;s/\\\$VER\\\$/$ver/g" "$head" > "$out"
+else
+    echo "Error: missing $head"
+    rc=1
+fi
+if [ ! -f $manifest ] ; then
+    echo "Error: missing $manifest"
+    rc=1
+fi
+[ 0 = "$rc" ] || exit $rc
+for file in `sed -e 's/#.*//g' $manifest` ; do
+    if [ -f $file ] ; then
+        sed "s/\\\$REV\\\$/$rev/g;s/\\\$VER\\\$/$ver/g" "$file" >> "$out"
+    else
+        echo "Error: missing $file"
+        rc=1
+    fi
+done
+[ 0 = "$rc" ] || exit $rc
 
 ### Google Chrome extension (unpacked) ###
 echo "Creating unpacked Chrome extension"
 # Create chrome build dir if doesn't exists
 mkdir -p chrome/GameGolem
 mkdir -p chrome/GameGolem/images
-cp -r chrome/GameGolem.tmpl/* chrome/GameGolem
-sed "s/\\\$REV\\\$/$rev/g;s/\\\$VER\\\$/$ver/g" chrome/manifest.tmpl > chrome/GameGolem/manifest.json
-cp  main.js \
-    utility.js \
-    worker.js \
-	golem.css \
-    $(ls -1 worker_+*.js) \
-    $(ls -1 worker_*.js | grep -v "\+") \
-	chrome/GameGolem/
+cp -r chrome/GameGolem.tmpl/* chrome/GameGolem/
 cp -r images/*.png chrome/GameGolem/images/
+cp golem.css chrome/GameGolem/
+in=chrome/manifest.tmpl
+out=chrome/GameGolem/manifest.json
+if [ -f "$in" ] ; then
+    sed "s/\\\$REV\\\$/$rev/g;s/\\\$VER\\\$/$ver/g" "$in" > "$out"
+else
+    echo "Error: missing $in"
+    rc=1
+fi
+for file in `sed -e 's/#.*//g' $manifest` ; do
+    if [ -f $file ] ; then
+        cp $file chrome/GameGolem/
+    else
+        echo "Error: missing $file"
+        rc=1
+    fi
+done
+[ 0 = "$rc" ] || exit $rc
 
 ### GOOGLE CHROME EXTENSION ###
 # To build the "proper" chrome extension you need Chrome installed
@@ -122,28 +166,40 @@ if [ "$build_chrome" = "Yes" ]; then
         "$chrome_browser" --no-message-box --pack-extension="$workdir/chrome/GameGolem" --pack-extension-key="$workdir/chrome/GameGolem.pem"
         sed "s/\\\$REV\\\$/$rev/g;s/\\\$VER\\\$/$ver/g" chrome/update.tmpl > chrome/update.xml
         cp _version.js chrome/
-   else 
-        echo "Would create packed Chrome extension, but you miss chrome/GameGolem.pem file"
+   else
+        echo "Would create packed Chrome extension, but you are missing chrome/GameGolem.pem"
     fi
 fi
 
 ### MINIMISED VERSION ###
+in=_normal.user.js
+out=_min.user.js
 if [ "$build_release" = "Yes" ]; then
     echo "Creating minimised version (will display any syntax errors)"
-    if [ -r "$js_compiler" ]; then
-      sed "s/\\\$REV\\\$/$rev/g;s/\\\$VER\\\$/$ver/g" _head.tmpl > _min.user.js
-      java -jar "$js_compiler" --js _normal.user.js >> _min.user.js
+    if [ -f "$js_compiler" -a -f "$in" ]; then
+	if [ -f $head ] ; then
+	    sed "s/\\\$REV\\\$/$rev/g;s/\\\$VER\\\$/$ver/g" "$head" > "$out"
+	else
+	    echo "Error: missing $head"
+	    rc=1
+	fi
+        java -jar "$js_compiler" --js "$in" >> "$out"
     else
-      echo "Error: missing js compiler."
+        echo "Error: missing js compiler."
+        rc=1
     fi
 fi
 
 ### INSTALLED VERSION ###
+in=_normal.user.js
 if [ "$update_firefox" = "Yes" ]; then
     echo "Installing new version to Firefox"
     for script in "$HOME"/.mozilla/firefox/*.default/gm_scripts/rycochets_castle_age_gol/rycochets_castle_age_gol.user.js; do
         if [ -f "$script" ]; then
-            cat _normal.user.js > "$script"
+            cat "$in" > "$script"
         fi
     done
 fi
+
+[ 0 = "$rc" ] && echo "Build completed."
+exit $rc
