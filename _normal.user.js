b279a5754c012fe99862343c3c7c491658a00b9d
RycochetTM <RycochetTM@98a03d04-1f35-11df-8982-67e453f37615>
2011-01-02 05:27:52 +0000

    NOTE - Firefox needs the updated css file, the build script will not update that so manually copy it over. Update more images.

--- _normal.user.js
+++ _normal.user.js
@@ -3,7 +3,7 @@
 // @namespace	golem
 // @description	Auto player for Castle Age on Facebook. If there's anything you'd like it to do, just ask...
 // @license		GNU Lesser General Public License; http://www.gnu.org/licenses/lgpl.html
-// @version		31.5.898
+// @version		31.5.900
 // @include		http://apps.facebook.com/castle_age/*
 // @include		https://apps.facebook.com/castle_age/*
 // @require		http://cloutman.com/jquery-1.4.2.min.js
@@ -26,7 +26,7 @@ var isRelease = false;
 var script_started = Date.now();
 // Version of the script
 var version = "31.5";
-var revision = 898;
+var revision = 900;
 // Automatically filled from Worker:Main
 var userID, imagepath, APP, APPID, APPNAME, PREFIX; // All set from Worker:Main
 // Detect browser - this is rough detection, mainly for updates - may use jQuery detection at a later point
